36b968a0efd085c01763d2c79d6eed5d41cc8b99
ztabassum <zohratab@gmail.com>
2023-05-09 17:48:04 -0500

    Update serve.py

--- serve.py
+++ serve.py
@@ -3,15 +3,16 @@ import subprocess
 import socket
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        subprocess.Popen(['python', 'stress_cpu.py'])
+        subprocess.Popen(['python3', 'stress_cpu.py'])
         return "Success"
     else:
         return str(socket.gethostname())
-    
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
